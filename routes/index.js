2af950edd1fff382d8582259cb8358617cb140a7
AlexKovalenkoBY <kovalenkoam@gmail.com>
2023-07-14 08:44:59 +0300

    root: __root+'mediplus'

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get("/", function (req, res, next) {
   // res.sendFil("index", { title: "Express app title", name: "Прыуэт!!!!!!!   ))))" });
-  res.sendFile('index.html', { root: __dirname+'mediplus' })
+  res.sendFile('index.html', { root: __root+'mediplus' })
 });
 
 module.exports = router;
